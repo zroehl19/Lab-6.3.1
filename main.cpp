e4325f7a6e52e334ab32dba7fa2e5170831a1e3e
zroehl <zroehl@192.168.2.1>
2017-10-27 14:45:35 -0400

    done

---
+++ main.cpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: zroehl
+ *
+ * Created on October 16, 2017, 1:34 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    cout << " /\\" << endl;
+    cout << "//\\\\" << "\n" << endl;
+    
+    cout << " /\\" << endl;
+    cout << "/**\\" << "\n" << endl;
+    
+    cout << " /\\" << endl;
+    cout << "/++\\" << endl;
+}
+
